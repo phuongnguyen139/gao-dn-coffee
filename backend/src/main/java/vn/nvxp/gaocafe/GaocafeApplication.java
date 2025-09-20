d3c33dcfd11c4679cdb5425d9137a037148318e8
phuongnguyen139 <bapcaisach@gmail.com>
2025-09-20 15:53:03 +0700

    first commit for cafe project

---
+++ backend/src/main/java/vn/nvxp/gaocafe/GaocafeApplication.java
@@ -0,0 +1,13 @@
+package vn.nvxp.gaocafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaocafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaocafeApplication.class, args);
+	}
+
+}
